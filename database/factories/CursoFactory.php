2e93fd80cf98eea8b48ea20e07492466461b1631
rayala83 <ragieci@gmail.com>
2021-08-18 18:59:27 -0400

    Add files via upload

---
+++ database/factories/CursoFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\curso;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CursoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = curso::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->sentence(),
+            'descripcion' => $this->faker->paragraph(),
+            'categoria' => $this->faker->randomElement(['Desarrollo Web', 'Diseño Web'])
+
+
+        ];
+    }
+}
