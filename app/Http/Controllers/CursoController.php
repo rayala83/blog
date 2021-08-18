1e1a79629150df1306a6259ce626092b5c0c3213
rayala83 <ragieci@gmail.com>
2021-08-18 18:54:43 -0400

    Add files via upload

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -2,17 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\curso;
 use Illuminate\Http\Request;
 
 class CursoController extends Controller
 {
     public function index(){
-        return view('cursos.index');
+        $cursos= curso::paginate();
+        
+        return view('cursos.index', compact('cursos'));
     }
     public function create(){
         return view('cursos.create');
     }
-    public function show($curso){
+
+
+    public function store(Request $request){
+        $curso = new curso();
+
+        $curso->name = $request->name;
+        $curso->descripcion = $request->descripcion;
+        $curso->categoria = $request->categoria;
+
+        $curso->save();
+
+        return redirect()->route('cursos.show',$curso);
+
+    }
+    
+    public function show($id){
+
+        $curso = curso::find($id);
+       
+
         return view('cursos.show',['curso' => $curso]);
+    }
+
+    public function edit(curso $curso){       
+
+        return view('cursos.edit', compact('curso'));
     }
 }
